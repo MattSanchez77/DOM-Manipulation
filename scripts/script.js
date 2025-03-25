2031ff5761cc398b656f3c557b677a7cdf56c8ac
MattSanchez77 <matthew.sanchez77@icloud.com>
2025-03-25 13:38:37 -0600

    added more changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,19 @@
 /*
-Select and cache the <main> element in a variable named mainEl.
-Set the background color of mainEl to the value stored in the --main-bg CSS custom property.
+1). Select and cache the <main> element in a variable named mainEl.
+2.) Set the background color of mainEl to the value stored in the --main-bg CSS custom property.
 Hint: Assign a string that uses the CSS var() function like this: 'var(--main-bg)'.
-Set the content of mainEl to <h1>DOM Manipulation</h1>. There are a variety of ways to do this; pick whichever one that you think works best in this situation.
-Add a class of flex-ctr to mainEl.
+3). Set the content of mainEl to <h1>DOM Manipulation</h1>. There are a variety of ways to do this; pick whichever one that you think works best in this situation.
+4). Add a class of flex-ctr to mainEl.
 Hint: Use the Element.classList API.
 */
 
+let mainEl = document.querySelector('main')
+console.log(mainEl);
 
+mainEl.style.backgroundColor = `var( --main-bg)`;
+
+mainEl.innerHTML = `<h1> DOM Manipulation </h1>`
+
+mainEl.classList.add('flex-ctr')
 
 
